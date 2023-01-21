406f96ef4d61ab99d88e1e058e7be9c449a45768
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-01-21 11:09:26 +0000

    finalising separating branches

--- routes/api-/reviews-router.js
+++ routes/api-/reviews-router.js
@@ -4,15 +4,18 @@ const {
   getReviews,
   getReviewsByReviewId,
   getCommentsByReviewId,
+  postCommentOnReviewId,
   patchReview,
-  postReview,
 } = require("../../controllers/app-controllers");
 
-reviewsRouter.route("/").get(getReviews).post(postReview);
+reviewsRouter.route("/").get(getReviews);
 
 reviewsRouter.get("/:review_id", getReviewsByReviewId);
 
-reviewsRouter.route("/:review_id/comments").get(getCommentsByReviewId);
+reviewsRouter
+  .route("/:review_id/comments")
+  .get(getCommentsByReviewId)
+  .post(postCommentOnReviewId);
 
 reviewsRouter.patch("/:review_id", patchReview);
 
